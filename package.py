dfb330e9e973055a2f9ac507f4693d1c9a78ab92
Allan Simonsen <allansimonsen1972@gmail.com>
2021-10-16 07:25:58 +0200

    Fixed bug in package.py

--- package.py
+++ package.py
@@ -35,7 +35,7 @@ def isChangeLogUpdatedWithPackageJsonVersion(packageJsonVersion) -> bool:
 def isAllPackagesInstalledLocally() -> bool:
     process = subprocess.Popen(["cmd", "/c", "npm", "list", "--production", "--parseable", "--depth=99999", "--loglevel=error"], stderr=subprocess.PIPE)
     out = process.stderr.read()
-    success = (len(out) == 0);
+    success = (len(out) == 0)
     if (not success):
       print(f"Error while checking if all packages is installed locally: {out}")
     return success
@@ -44,7 +44,7 @@ def isAllPackagesInstalledLocally() -> bool:
 def packageExtension() -> bool:
     process = subprocess.Popen(["cmd", "/c", "vsce", "package"], stderr=subprocess.PIPE)
     out = process.stderr.read()
-    success = (len(out) == 0);
+    success = (len(out) == 0)
     if (not success):
       print(f"Error while packaging: {out}")
     return success
